6bf4b2a3acf2869856bcdc40293f253481633bfa
tan-nate <nktan93@gmail.com>
2019-08-30 09:47:34 -0700

    working on sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :quotes
   resources :users
   resources :topics
+  resources :sessions
+
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#delete'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
