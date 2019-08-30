eabd755b3aaf1c1e9526c53ae140d2035e71fa2a
tan-nate <nktan93@gmail.com>
2019-08-30 10:31:47 -0700

    working on sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
-
+    def new
+    end
     
+    def create
+        @user = User.find_by(username: params[:username])
+        return head(:forbidden) unless @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :topics
   resources :sessions
 
+  root to: 'topics#index'
+
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   get '/logout' => 'sessions#delete'
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
