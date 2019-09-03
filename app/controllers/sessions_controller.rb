0a0d14fcf4ccdfd82561e2cb126a30f42359543a
tan-nate <nktan93@gmail.com>
2019-09-02 19:17:18 -0700

    I made some good progress.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,9 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to root_path
     end
+
+    def delete
+        session.delete :user_id
+        redirect_to root_path
+    end
 end
