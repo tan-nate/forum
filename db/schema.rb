d9c331bae48d7bab336b37c22f4917efcdaa4985
tan-nate <nktan93@gmail.com>
2019-09-07 12:45:31 -0700

    made quotes belong_to topic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_193804) do
+ActiveRecord::Schema.define(version: 2019_09_07_194115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_193804) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "source_url"
+    t.integer "topic_id"
   end
 
   create_table "supports", force: :cascade do |t|
