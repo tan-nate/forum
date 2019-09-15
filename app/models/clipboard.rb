7b370e733567393358493f6167521d43808866de
tan-nate <nktan93@gmail.com>
2019-09-14 17:08:07 -0700

    clipboard organizes by topic

--- app/models/clipboard.rb
+++ app/models/clipboard.rb
@@ -2,4 +2,16 @@ class Clipboard < ApplicationRecord
     belongs_to :user
     has_many :clipboard_quotes
     has_many :quotes, through: :clipboard_quotes
+
+    def clipboard_topics
+        topics = quotes.collect do |quote|
+            quote.topic
+        end
+        topics.uniq!
+        topics
+    end
+
+    def clipboard_topic_quotes(topic)
+        quotes.select {|q| q.topic == topic}
+    end
 end
