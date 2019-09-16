35a05899d4fdc4499b5dd8afb958f8458c3aaa79
tan-nate <nktan93@gmail.com>
2019-09-15 23:07:33 -0700

    made some good progress

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,8 @@ class Place < ApplicationRecord
     has_many :user_places
     has_many :users, through: :user_places
     has_many :topics
+
+    def first_3_topics
+        self.topics.limit(3).to_a
+    end
 end
