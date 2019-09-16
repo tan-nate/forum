1518a7f3691aaa4a85b8b393584124b54549d31d
tan-nate <nktan93@gmail.com>
2019-09-16 08:25:43 -0700

    places show, places index, aesthetic changes

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
     has_many :users, through: :user_places
     has_many :topics
 
-    def first_3_topics
-        self.topics.limit(3).to_a
+    def first_5_topics
+        self.topics.limit(5).to_a
     end
 end
