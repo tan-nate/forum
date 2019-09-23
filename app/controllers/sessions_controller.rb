07890e40ee4d68f6ed147dbabbafe639e191f2bb
tan-nate <nktan93@gmail.com>
2019-09-22 22:55:23 -0700

    final app, facebook login still not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,9 @@ class SessionsController < ApplicationController
     
     def create
         if auth
-            @user = User.find_or_create_by(uid: auth['uid']) do |u|
-                u.username = namify(auth['info']['name'])
-            end
-        
+            @user = User.find_or_create_by(uid: auth['uid'])
+            @user.username = namify(auth[:info][:name])
             session[:user_id] = @user.id
-        
             redirect_to root_path
         else
             @user = User.find_by(username: params[:user][:username])
@@ -26,5 +23,11 @@ class SessionsController < ApplicationController
     def delete
         session.delete :user_id
         redirect_to root_path
+    end
+
+    private
+ 
+    def auth
+        request.env['omniauth.auth']
     end
 end
