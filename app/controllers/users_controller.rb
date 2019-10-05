cc3a1663c72a0c96fac27c1bfbf351414296cd94
tan-nate <nktan93@gmail.com>
2019-10-04 19:03:28 -0700

    error explanations for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
                 @user.update(clipboard: Clipboard.create)
                 session[:user_id] = @user.id
                 redirect_to root_path
+            else
+                render :new
             end
         end
     end
